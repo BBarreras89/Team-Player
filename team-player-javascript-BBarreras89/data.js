bf755d03bf3a010b6d44103a2ac8c8073388d9d0
BBarreras89 <88046459+BBarreras89@users.noreply.github.com>
2023-02-17 13:12:24 +0100

    Add files via upload

---
+++ team-player-javascript-BBarreras89/data.js
@@ -0,0 +1,396 @@
+const ourTeam = {
+  name: "Awesome Software Inc.",
+  members: [
+    {
+      name: "Rodrigo",
+      colours: {
+        primary: {
+          red: 244,
+          green: 109,
+          blue: 97,
+        },
+        secondary: {
+          red: 0,
+          green: 0,
+          blue: 0,
+        },
+      },
+      birthday: {
+        year: 1990,
+        month: 11,
+        day: 17,
+      },
+      location: {
+        country: "Spain",
+        city: "Saragosa",
+      },
+      codinglvl: 10,
+      favorites: {
+        favMovies: [
+          {
+            title: "Dredd",
+            year: 2012,
+            rating: 7.1,
+            directors: "Pete Travis",
+            writers: "Alex Garland",
+            stars: ["Karl Urban,Lena Headey:"],
+            genres: "Science-Fiction-Action",
+            review:
+              "Awesome actionfilled movie that respects and sticks to the source material.",
+          },
+          {
+            title: "New Police Story",
+            year: 2004,
+            rating: 6.9,
+            directors: "Benny Chan",
+            writers: "Alan Yuen",
+            stars: ["Jackie Chan", "Nicholas Tse"],
+            genres: "Action",
+            review:
+              "It's not your usual Jackie Chan action-comedy, alot more darker and a pretty good story.",
+          },
+          {
+            title: "Jurassic Park",
+            year: 1993,
+            rating: 8.2,
+            directors: "Steven Spielberg",
+            writers: "Michael Crichton, David Koepp",
+            stars: [
+              "Richard Attenborough, Sam Neill,Laura Dern, Jeff Goldblum",
+            ],
+            genres: "Science-Fiction- und Abenteuer",
+            review: "Plain and simple, it's a masterpiece",
+          },
+        ],
+
+        favBooks: [
+          {
+            title: "Lord of the Rings",
+            author: "JRR Tolkien",
+            year: 1954,
+            characters: ["Gandalf", "Gollum", "Gimli", "Saruman"],
+            review: "Deep lore in a well thought fantasy world",
+          },
+          {
+            title: "A Game Of Thrones",
+            author: "George R. R. Martin",
+            year: 2001,
+            characters: [
+              "Tyrion Lannister the Imp",
+              "Oberyn Martell the Red Viper",
+              "Sandor Clegane the Hound",
+              "Lord Varys the Spider",
+            ],
+            review: "My absolute favorite bookseries ever, it can't be beaten.",
+          },
+        ],
+        favSongs: [
+          {
+            title: "The Dark Side of the Moon",
+            artist: "Pink Floyd",
+            genres: ["progressive rock"],
+            review: "Old classic.",
+          },
+          {
+            title: "Finisterra",
+            artist: "Mago de Oz",
+            genres: ["Celtiberian Metal", "Folk Metal"],
+            review: "When you are in the mood for something different",
+          },
+          {
+            title: "Very Impressive",
+            artist: "Davide Biale",
+            genres: ["Electronic", "Funk / Soul"],
+            review: "BASS!",
+          },
+        ],
+      },
+    },
+    {
+      name: "Fatih",
+      colours: {
+        primary: {
+          red: 244,
+          green: 109,
+          blue: 97,
+        },
+        secondary: {
+          red: 0,
+          green: 0,
+          blue: 0,
+        },
+      },
+      birthday: {
+        year: 1979,
+        month: 10,
+        day: 1,
+      },
+      location: {
+        country: "Österreich",
+        city: "Wien",
+      },
+      codinglvl: 2,
+      favorites: {
+        favMovies: [
+          {
+            title: "La vita e bella",
+            year: 1997,
+            rating: 8.6,
+            review: "Supa!",
+          },
+          {
+            title: "La Haine",
+            year: 1995,
+            rating: 8.1,
+            review: "Sehr supa",
+          },
+          {
+            title: "Fahrenheit 451",
+            year: 1966,
+            rating: 7.2,
+            review: "Top film",
+          },
+        ],
+        favBooks: [
+          {
+            title: "Kafka on the shore",
+            author: "Haruki Murikami",
+            year: 2005,
+            characters: ["Kathrin", "Thomas"],
+            review: "Kann man immerwieder lesen",
+          },
+          {
+            title: "In search of lost time",
+            author: "Marcel Proust",
+            year: 2004,
+            characters: ["Arthur", "Cathrina"],
+            review: "Verlorene Zeit",
+          },
+        ],
+        favSongs: [
+          {
+            title: "The Dark Side of the Moon",
+            artist: "Pink Floyd",
+            genres: ["Progressive rock"],
+            review: "Old classic",
+          },
+          {
+            title: "Finisterra",
+            artist: "Mago de Oz",
+            genres: ["Celtiberian Metal", "Folk Metal"],
+            review: "When you are in the mood for something different",
+          },
+          {
+            title: "Very Impressive",
+            artist: "Davide Biale",
+            genres: ["Electronic", "Funk / Soul"],
+            review: "BASS",
+          },
+        ],
+      },
+    },
+    {
+      name: "Benito",
+      colours: {
+        primary: {
+          red: 244,
+          green: 109,
+          blue: 97,
+        },
+        secondary: {
+          red: 0,
+          green: 0,
+          blue: 0,
+        },
+      },
+      birthday: {
+        year: 1989,
+        month: "September",
+        day: 13,
+      },
+      location: {
+        country: "Cuba",
+        city: "Camagüey",
+      },
+      codinglvl: 80,
+      favorites: {
+        favMovies: [
+          {
+            title: "John Wick 3: Parabellum",
+            year: 2019,
+            rating: 7.4,
+            review:
+              "One of the best action movies of all time.The use of light is appealing and you don't have this shaky camera, where you can't follow the action sequences.",
+          },
+          {
+            title: "Heat",
+            year: 1995,
+            rating: 8.3,
+            review:
+              "It is a classic. Really good acted, with amazing actors and tense action sequences.",
+          },
+          {
+            title: "A Walk to Remember",
+            year: 2002,
+            rating: 7.3,
+            review: "A good love story with not your everyday happy ending.",
+          },
+          {
+            title: "Glory Road",
+            year: 2006,
+            rating: 7.2,
+            review:
+              "Basketball-movie about the first collegeteam to play and win the NCAA Championship with just black players.",
+          },
+        ],
+        favBooks: [
+          {
+            title: "Ready Player One",
+            author: "Ernest Cline",
+            year: 2011,
+            age: 2011,
+            characters: [
+              "Wade Watts",
+              "Samantha Cook",
+              "Aech",
+              "Ogden Morrow ",
+            ],
+            review: "Cool adventure book, with lots of pop-culture references.",
+          },
+          {
+            title: "House of M",
+            author: "Brian Michael Bendis",
+            year: 2005,
+            age: 2011,
+            characters: [
+              "James Howlett alias aka Wolverine",
+              "Wanda Romanov aka Scarlett Witch",
+              "Dr Steven Strang aka Doctor Strange",
+              "Charles Xavier",
+            ],
+            review: "Extremely important comic for the marvel universe.",
+          },
+        ],
+        favSongs: [
+          {
+            title: "Mi tierra",
+            artist: "Gloria Estefan",
+            genres: ["Salsa"],
+            review: "Classic salsamusic, praising ones birth-country.",
+          },
+          {
+            title: "Aint leaving without you",
+            artist: "Jaheim",
+            genres: ["Soul", "RNB"],
+            review: "Good vibes, oldschool RNB.",
+          },
+          {
+            title: "It Was A Good Day",
+            artist: "Ice Cube",
+            genres: ["Hip Hop", "Rap"],
+            review:
+              "good vibes, oldschool rap, relaxed and chilled for having a good day.",
+          },
+        ],
+      },
+    },
+  ],
+};
+
+//averageAge
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let averageAge = 0;
+
+for (let member of ourTeam.members) {
+  averageAge += (currentYear - member.birthday.year) / ourTeam.members.length;
+};
+console.log(averageAge);
+
+//averageCodingLevel
+let averageCodingLevel = 0;
+
+for (let member of ourTeam.members) {
+  averageCodingLevel += member.codinglvl / ourTeam.members.length;
+};
+console.log(averageCodingLevel.toFixed(2));
+
+//Same Location check
+let sameLocations = 0;
+let location = {};
+
+for (let member of ourTeam.members) {
+  sameLocations = member.location.city;
+  //checking for doubles
+  let cityDoubles = location[sameLocations] !== undefined;
+  //members.name to the keys of the city names
+  if (cityDoubles) {
+    location[sameLocations] += ", " + member.name + ". ";
+  } else {
+    location[sameLocations] = member.name;
+  };
+};
+console.log(location);
+
+
+//commonMusic styles
+let commonGenres = {};
+
+for (let member of ourTeam.members) {
+  for (let song of member.favorites.favSongs)
+    for (let genre of song.genres) {
+      if (commonGenres[genre]) {
+        commonGenres[genre] += 1;
+      } else {
+        commonGenres[genre] = 1;
+      };
+    };
+};
+
+console.log(commonGenres);
+
+// youngestMember
+let youngestYear = ourTeam.members[0].birthday.year;
+let youngestMember = ourTeam.members[0].name;
+
+for (const member of ourTeam.members) {
+  if (member.birthday.year > youngestYear) {
+    youngestYear = member.birthday.year;
+    youngestMember = member.name;
+  };
+};
+console.log("Our youngest teammember is " + youngestMember + ".");
+
+//oldestMember
+let oldestYear = ourTeam.members[0].birthday.year;
+let oldestMember = ourTeam.members[0].name;
+
+for (const member of ourTeam.members) {
+  if (member.birthday.year < oldestYear) {
+    oldestYear = member.birthday.year;
+    oldestMember = member.name;
+  };
+};
+console.log("Our oldest teammember is " + oldestMember + ".");
+
+// DON'T MODIFY THE CODE BELOW THIS LINE
+
+let toExport;
+
+try {
+  //{ourTeam, averageAge, youngestMember, location, }
+  toExport = [
+    { name: "ourTeam", content: ourTeam, type: "object" },
+    { name: "averageAge", content: averageAge, type: "number" },
+    { name: "averageCodingLevel", content: averageCodingLevel, type: "number" },
+    { name: "youngestMember", content: youngestMember, type: "string" },
+    { name: "oldestMember", content: oldestMember, type: "string" },
+    { name: "location", content: location, type: "object" },
+    { name: "commonGenres", content: commonGenres, type: "object" },
+    /* { name: "moreStats", content: moreStats, type: "object" } // remove the block comment from this line if you want to make the last optional task */
+  ];
+} catch (error) {
+  toExport = { error: error.message };
+}
+
+export { toExport };
